239c2a93cb06f3be2e3c16dd1c985afc74c9fc4d
cristiano230981 <vbscristiano@gmail.com>
2022-08-07 07:54:41 -0300

    feat: add SignUp Factory

--- src/infra/criptography/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/criptography/db/mongodb/helpers/mongo-helper.ts
@@ -2,8 +2,11 @@ import { Collection, MongoClient } from "mongodb"
 
 export const MongoHelper = {
     client: null as MongoClient,
+    uri: null as string,
+
     async connect (uri: string): Promise<void> {
-        this.client = await MongoClient.connect(process.env.MONGO_URL)
+        this.uri = uri
+        this.client = await MongoClient.connect(uri)
     },
     async disconnect () {
         await this.client.close()
