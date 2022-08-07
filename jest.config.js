88cedfca7a8945438510e4a99dfb36bff6516090
cristiano230981 <vbscristiano@gmail.com>
2022-08-07 16:11:48 -0300

    feat: ensure MongoDb reconnect if client is down

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts',
+  '!<rootDir>/src/main/**'],
   coverageDirectory: "coverage",
   transform: {
    '.+\\.ts$' : 'ts-jest'
--- src/infra/criptography/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/criptography/db/mongodb/helpers/mongo-helper.ts
@@ -9,9 +9,13 @@ export const MongoHelper = {
         this.client = await MongoClient.connect(uri)
     },
     async disconnect () {
-        await this.client.close()
+        //await this.client.close()
+        this.client = null
     },
-    getCollection (name: string): Collection {
+    async getCollection (name: string): Promise<Collection> {
+        if(!this.client == null){
+            await this.client.connect(this.uri)
+        }
         return this.client.db().collection(name)
     },
     map: (connection: any, id: any): any => {
