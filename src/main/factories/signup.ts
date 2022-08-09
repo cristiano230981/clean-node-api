847aca96317cdf243c42d144dce9cacd80176e09
cristiano230981 <vbscristiano@gmail.com>
2022-08-09 10:30:24 -0300

    refactor: make correct type to makeSignUpController

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,11 +1,12 @@
-import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
-import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
+import { DbAddAccount           } from "../../data/usecases/add-account/db-add-account";
+import { BcryptAdapter          } from "../../infra/criptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/criptography/db/mongodb/account-repository/account";
-import { SignupController } from "../../presentation/controllers/signup/signup";
-import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+import { SignupController       } from "../../presentation/controllers/signup/signup";
+import { Controller             } from "../../presentation/protocols";
+import { EmailValidatorAdapter  } from "../../utils/email-validator-adapter";
 import { LogControllerDecorator } from "../decorators/log";
 
-export const  makeSignUpController = (): SignupController => {
+export const  makeSignUpController = (): Controller => {
     const salt = 12
     const emailValidatorAdapter = new EmailValidatorAdapter()
     const bcryptAdapter = new BcryptAdapter(salt)
