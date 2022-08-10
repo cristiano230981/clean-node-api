0695031ff6adaa0b1a2fd073ae98f5c716ebf1d6
cristiano230981 <vbscristiano@gmail.com>
2022-08-10 07:22:43 -0300

    refactor: make ServerError return the error stack

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -37,7 +37,7 @@ export class SignupController implements Controller {
 
             return ok(account)
         } catch (error) {
-            return serverError()
+            return serverError(error)
         }
     }
 }
