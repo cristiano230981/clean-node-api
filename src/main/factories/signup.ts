9b3662ba758bd80836d399df2fb90d73d75a70a2
cristiano230981 <vbscristiano@gmail.com>
2022-08-12 11:53:50 -0300

    refactor: move required field validation to the ValidationComposite

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,11 +1,12 @@
 import { DbAddAccount           } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter          } from "../../infra/criptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
-import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log";
+import { LogMongoRepository     } from "../../infra/db/mongodb/log-repository/log";
 import { SignupController       } from "../../presentation/controllers/signup/signup";
 import { Controller             } from "../../presentation/protocols";
 import { EmailValidatorAdapter  } from "../../utils/email-validator-adapter";
 import { LogControllerDecorator } from "../decorators/log";
+import { makeSignUpValidation   } from "./signup-validation";
 
 export const  makeSignUpController = (): Controller => {
     const salt = 12
@@ -13,7 +14,7 @@ export const  makeSignUpController = (): Controller => {
     const bcryptAdapter = new BcryptAdapter(salt)
     const accountMongoRepository = new AccountMongoRepository()
     const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-    const signUpController = new SignupController(emailValidatorAdapter, dbAddAccount)
+    const signUpController = new SignupController(emailValidatorAdapter, dbAddAccount, makeSignUpValidation())
     const logMongoRepository = new LogMongoRepository()
     return new LogControllerDecorator(signUpController, logMongoRepository)
 }
