2fdba974bd83b5d0534452abec91f22bcf91d843
cristiano230981 <vbscristiano@gmail.com>
2022-08-15 10:58:24 -0300

    feat: ensure AccountMongoRepository returns an Account on loadByEmail success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,18 +3,22 @@ import { Collection, MongoClient } from "mongodb"
 export const MongoHelper = {
     client: null as MongoClient,
     uri: null as string,
+    connected: null as boolean,
 
     async connect (uri: string): Promise<void> {
         this.uri = uri
         this.client = await MongoClient.connect(uri)
+        this.connected = true
     },
     async disconnect () {
         await this.client?.close()
         this.client = null
+        this.connected = false
     },
     async getCollection (name: string): Promise<Collection> {
-        if(this.client == null){
+        if(this.client == null || this.connected == null || !this.connected){
             await this.client.connect(this.uri)
+            this.connected = true
         }
         return this.client.db().collection(name)
     },
