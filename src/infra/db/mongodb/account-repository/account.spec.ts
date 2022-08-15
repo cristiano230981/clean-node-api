8c1cdbc734e2319648c9c3d5b492e228af5ac6ea
cristiano230981 <vbscristiano@gmail.com>
2022-08-15 10:58:42 -0300

    test: ensure AccountMongoRepository returns an Account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -19,7 +19,7 @@ describe('Account Mongo Repository', () => {
         return new AccountMongoRepository()
     }
 
-    test('Should return account on success', async () => {
+    test('Should return account on add success', async () => {
         const sut = makeSut()
         const account = await sut.add({
           name: 'any_name',
@@ -32,4 +32,20 @@ describe('Account Mongo Repository', () => {
         expect(account.email).toBe('any_email@email.com')
         expect(account.password).toBe('any_password')
     })
+
+    test('Should return account on loadByEmail success', async () => {
+        const sut = makeSut()
+        await sut.add({
+            name: 'any_name',
+            email: 'any_email@email.com',
+            password: 'any_password'
+        })
+        const account = await sut.loadByEmail('any_email@email.com')
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toBe('any_name')
+        expect(account.email).toBe('any_email@email.com')
+        expect(account.password).toBe('any_password')
+    })
+
 })
