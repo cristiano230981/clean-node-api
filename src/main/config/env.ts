61a220faea36b50fa50ebde949d1623892dc800b
cristiano230981 <vbscristiano@gmail.com>
2022-08-17 08:16:06 -0300

    refactor: set port number on config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
     mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-    port: process.env.port || 5050,
+    port: process.env.port || 3000,
     jwtScret: process.env.JWT_SECRET || '#843jkds9432-3',
 }
