34eb7a7ecf6d6eb381ff8acc576c3f83ed75f483
cristiano230981 <vbscristiano@gmail.com>
2022-08-24 19:40:44 -0300

    feat: ensure AddSurveyController returns 204 on success

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -35,3 +35,10 @@ export const ok = (data: any): HttpResponse => (
        body: data
    }
 )
+
+export const noContent = (): HttpResponse => (
+    {
+       statusCode: 204,
+       body: null
+   }
+)
