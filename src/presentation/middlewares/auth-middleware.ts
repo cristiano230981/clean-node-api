e8dfd2787424e39a1e12567469d6999034d65598
cristiano230981 <vbscristiano@gmail.com>
2022-08-28 08:45:56 -0300

    feat: ensuce AuthMiddleware call LoadAccountByToken with correct values

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,12 +1,18 @@
+import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token";
 import { AccessDeniedError } from "../errors";
 import { forbidden } from "../helpers/http/http-helper";
 import { HttpRequest, HttpResponse } from "../protocols";
 import { Middleware } from "../protocols/middleware";
 
 export class AuthMiddleware implements Middleware {
+    constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
+
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const error = forbidden(new AccessDeniedError())
-        return new Promise(resolve => resolve(error))
+        const accessToken = httpRequest.headers?.['x-access-token']
+        if (accessToken ) {
+            await this.loadAccountByToken.load(accessToken)
+        }
+        return forbidden(new AccessDeniedError())
     }
 
 }
