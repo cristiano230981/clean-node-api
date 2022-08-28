2213d817d614eb64521dda00a372efc3c62b28d9
cristiano230981 <vbscristiano@gmail.com>
2022-08-28 08:32:18 -0300

    test: ensure AuthMiddleware returns 403 if no headers are found

---
+++ src/presentation/middlewares/auth-middleware.ts
@@ -0,0 +1,12 @@
+import { AccessDeniedError } from "../errors";
+import { forbidden } from "../helpers/http/http-helper";
+import { HttpRequest, HttpResponse } from "../protocols";
+import { Middleware } from "../protocols/middleware";
+
+export class AuthMiddleware implements Middleware {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const error = forbidden(new AccessDeniedError())
+        return new Promise(resolve => resolve(error))
+    }
+
+}
