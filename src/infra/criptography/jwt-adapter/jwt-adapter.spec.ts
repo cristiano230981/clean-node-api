2a47b4d554ea7a4a4e5e5147d3cd5a2956375290
cristiano230981 <vbscristiano@gmail.com>
2022-08-29 14:24:44 -0300

    test: ensure JwtAdapter returns a decrypted value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -47,5 +47,10 @@ describe('Jwt Adapter', () => {
             expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
         })
 
+        test('Should return a token on verify success', async () => {
+            const sut = makeSut()
+            const value = await sut.decrypt('any_token')
+            expect(value).toBe('any_value')
+        })
     })
 })
